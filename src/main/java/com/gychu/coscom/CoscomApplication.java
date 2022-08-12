368b01fde7ad7d11170ad0200c8576610dc961cd
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-11 23:56:41 -0400

    initial commit

---
+++ src/main/java/com/gychu/coscom/CoscomApplication.java
@@ -0,0 +1,13 @@
+package com.gychu.coscom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoscomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoscomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gychu/coscom/CoscomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gychu.coscom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoscomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
