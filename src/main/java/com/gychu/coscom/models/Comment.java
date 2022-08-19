d307a76a7dc7a37bdb5a74b5dfdc2c7cb7b8696d
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-19 16:12:56 -0400

    Added fields to Comment and Post

--- src/main/java/com/gychu/coscom/models/Comment.java
+++ src/main/java/com/gychu/coscom/models/Comment.java
@@ -1,7 +1,12 @@
 package com.gychu.coscom.models;
 
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Entity
 public class Comment {
+
+    String text;
+    User user;
+    LocalDate date;
 }
--- src/main/java/com/gychu/coscom/models/Post.java
+++ src/main/java/com/gychu/coscom/models/Post.java
@@ -1,7 +1,15 @@
 package com.gychu.coscom.models;
 
 import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Post {
+
+    String image;
+    String text;
+    List<Comment> comments;
+    User user;
+    LocalDate date;
 }
