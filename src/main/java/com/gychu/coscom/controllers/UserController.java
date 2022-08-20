d96a9dc58060bee1a1048d42412d637921ad9442
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-20 14:35:37 -0400

    Added UserController methods.

--- src/main/java/com/gychu/coscom/controllers/UserController.java
+++ src/main/java/com/gychu/coscom/controllers/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/Users")
+@RequestMapping("/user")
 public class UserController {
 
     //dependency injection for UserService
@@ -21,8 +21,23 @@ public class UserController {
         return userService.saveUser(user); //returning the User, so you can see what got saved.
     }
 
-    @GetMapping("/{username}")
-    private User getUserDetailsByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    @GetMapping
+    private List<User> getAllUsers() {
+        return userService.getAllUsers(); //returning the List of all Users.
+    }
+
+    @GetMapping ("/{username}")
+    private User getUserByUsername(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username); //returning User with username.
+    }
+
+    @PutMapping("/{username}")
+    private User updateUser(String username, User user) {
+        return userService.updateUser(username, user);
+    }
+
+    @DeleteMapping("/{username}")
+    private Boolean deleteUser(@PathVariable("username") String username) {
+        return userService.deleteUser(username);
     }
 }
