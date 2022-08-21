0b7ffdd43812ead385baa2046cc343dc186527cf
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-21 00:41:11 -0400

    Added Comment and Post repositories.

---
+++ src/main/java/com/gychu/coscom/repositories/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.gychu.coscom.repositories;
+
+import com.gychu.coscom.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Comment findByUser(String user);
+}
---
+++ src/main/java/com/gychu/coscom/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.gychu.coscom.repositories;
+
+import com.gychu.coscom.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Post findPostByUser(String user);
+}
