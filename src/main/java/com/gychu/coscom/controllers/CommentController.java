cb9698611ed0e6f647947a37b1ca7d19f011c753
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-21 20:59:35 -0400

    Updated CommentController methods to return ResponseEntity

--- src/main/java/com/gychu/coscom/controllers/CommentController.java
+++ src/main/java/com/gychu/coscom/controllers/CommentController.java
@@ -3,6 +3,8 @@ package com.gychu.coscom.controllers;
 import com.gychu.coscom.models.Comment;
 import com.gychu.coscom.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +17,27 @@ public class CommentController {
     CommentService commentService;
 
     @PostMapping
-    private Comment saveComment(@RequestBody Comment comment) {
-        return commentService.saveComment(comment);
+    private ResponseEntity<Comment> saveComment(@RequestBody Comment comment) {
+        return new ResponseEntity<>(commentService.saveComment(comment), HttpStatus.CREATED);
     }
 
     @GetMapping
-    private List<Comment> getAllComments() {
-        return commentService.getAllComments();
+    private ResponseEntity<List<Comment>> getAllComments() {
+        return new ResponseEntity<>(commentService.getAllComments(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    private Comment getCommentById (@PathVariable Long id) {
-        return commentService.getCommentById(id);
+    private ResponseEntity<Comment> getCommentById (@PathVariable Long id) {
+        return new ResponseEntity<>(commentService.getCommentById(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    private Comment updateComment (@PathVariable Long id, @RequestBody Comment comment) {
-        return commentService.updateComment(id, comment);
+    private ResponseEntity<Comment> updateComment (@PathVariable Long id, @RequestBody Comment comment) {
+        return new ResponseEntity<>(commentService.updateComment(id, comment), HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/{id}")
-    private Boolean deleteComment (@PathVariable Long id) {
-        return commentService.deleteComment(id);
+    private ResponseEntity<Boolean> deleteComment (@PathVariable Long id) {
+        return new ResponseEntity<>(commentService.deleteComment(id), HttpStatus.NO_CONTENT);
     }
 }
