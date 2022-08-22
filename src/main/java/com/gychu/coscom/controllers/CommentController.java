79e9133cf6f64e1a49476f19e0c8d305d6e1356f
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-21 21:07:00 -0400

    Added try/catch to CommentController

--- src/main/java/com/gychu/coscom/controllers/CommentController.java
+++ src/main/java/com/gychu/coscom/controllers/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/comment")
@@ -27,17 +28,29 @@ public class CommentController {
     }
 
     @GetMapping("/{id}")
-    private ResponseEntity<Comment> getCommentById (@PathVariable Long id) {
-        return new ResponseEntity<>(commentService.getCommentById(id), HttpStatus.OK);
+    private ResponseEntity<Object> getCommentById (@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(commentService.getCommentById(id), HttpStatus.OK);
+        } catch (IllegalArgumentException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/{id}")
-    private ResponseEntity<Comment> updateComment (@PathVariable Long id, @RequestBody Comment comment) {
-        return new ResponseEntity<>(commentService.updateComment(id, comment), HttpStatus.ACCEPTED);
+    private ResponseEntity<Object> updateComment (@PathVariable Long id, @RequestBody Comment comment) {
+        try {
+            return new ResponseEntity<>(commentService.updateComment(id, comment), HttpStatus.ACCEPTED);
+        } catch (IllegalArgumentException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
-    private ResponseEntity<Boolean> deleteComment (@PathVariable Long id) {
-        return new ResponseEntity<>(commentService.deleteComment(id), HttpStatus.NO_CONTENT);
+    private ResponseEntity<Object> deleteComment (@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(commentService.deleteComment(id), HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
